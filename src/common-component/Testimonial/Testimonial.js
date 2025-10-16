5eb628538770055f1ea4be53d16425744a9f6198
rajul-09 <rajuldhimole9@gmail.com>
2025-10-16 22:13:21 +0530

    2nd section of contact us page

--- src/common-component/Testimonial/Testimonial.js
+++ src/common-component/Testimonial/Testimonial.js
@@ -1,20 +1,58 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
-const Testimonial = ({ icon, title, partner, description }) => {
+const Testimonial = ({ testimonialData }) => {
     return (
-        <div className="max-w-4xl mx-auto bg-[#fd4d40]  shadow p-7 text-center flex flex-col items-center hover:shadow cursor-pointer">
-            <div className='flex justify-between'>
-                <div className='w-32 flex-initial justify-items-start'>
-                    <img src={icon} alt={title} className="w-25 h-25 object-contain" />
-                </div>
-                <div className='w-64 flex-initial'>
-                    <h3 className="dm_sans responsiveheading6 font-semibold mb-5">{title}</h3>
-                    <p className='dm_sans responsive-text font-light text-[#6c757d] mb-2 md:mb-3 lg:mb-8 leading-6! lg:leading-8!'>{partner}</p>
-                    <p className='dm_sans responsive-text font-light text-[#6c757d] mb-2 md:mb-3 lg:mb-8 leading-6! lg:leading-8!'>{description}</p>
-                </div>
+        <div className="custom-container m-6 md:m-10 text-center ">
+            <p className='indie_flower font-light responsiveheading6 text-[#fd4d40] mb-1 leading-1.5 '>{`Testimonial`}</p>
+            <h2 className='dm_sans responsiveheading2 font-light mb-6 leading-1.5 '>{`What Our Client Says About Us`}</h2>
+            <div className="relative max-w-4xl mx-auto bg-[#fd4d40] shadow p-12 text-center flex flex-col items-center cursor-pointer rounded-lg overflow-hidden">
+                <Splide
+                    options={{
+                        type: "loop",
+                        perPage: 1,
+                        autoplay: true,
+                        interval: 3000,
+                        pauseOnHover: true,
+                        gap: "1.5rem",
+                        arrows: false,
+                        pagination: true,
+                      
+                    }}
+                    aria-label="Client Testimonials"
+                    className="w-full"
+                >
+                    {testimonialData?.map(({ icon, title, partner, description }, index) => (
+                        <SplideSlide key={index}>
+                            <div key={index} className="flex flex-col sm:flex-row justify-between w-full mb-6">
+                                <div className="w-32 flex-initial flex justify-center sm:justify-start">
+                                    <img
+                                        src={icon}
+                                        alt={title}
+                                        className="w-24 h-24 object-contain"
+                                    />
+                                </div>
+                                <div className="flex-1 text-left pl-6">
+                                    <h3 className="dm_sans responsiveheading6 font-semibold mb-2 text-white">
+                                        {title}
+                                    </h3>
+                                    <p className="dm_sans responsive-text font-light text-red-300 mb-3">
+                                        {partner}
+                                    </p>
+                                    <p className="dm_sans responsiveheading6 font-light text-gray-100 leading-10 pr-10 ">
+                                        {description}
+                                    </p>
+                                </div>
+                            </div>
+                        </SplideSlide>
+                    ))}
+                </Splide>
             </div>
         </div>
-    )
-}
 
-export default Testimonial
+    );
+};
+
+export default Testimonial;
