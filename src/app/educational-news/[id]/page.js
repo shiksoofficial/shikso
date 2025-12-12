6a8d6df25894bfff7de53974c3cbd42bce54a674
rajul-09 <rajuldhimole9@gmail.com>
2025-12-12 18:29:28 +0530

    single news/blog banner and footer

--- src/app/educational-news/[id]/page.js
+++ src/app/educational-news/[id]/page.js
@@ -68,11 +68,14 @@ const EducationalNewsDesc = async ({ params }) => {
   const data = await axios.get(`${BASE_URL_API}blogs/${id}/ed_tech`)
   return (
     <div>
-      <HeroSection imageurl={data?.data?.blog?.featuredImage?.url} title="Welcome to Our Website" 
-      title2= {data?.data?.blog?.title} button={false}
+      <HeroSection image={data?.data?.blog?.featuredImage?.url}
+        title={data?.data?.blog?.title}
+        title2={false}
+        showPrimaryBtn={false}
+        showSecondaryBtn={false}
         breadcom={[
           { title: "News", url: "/educational-news" },
-          { title: data?.data?.blog?.uid || "News Detail"},
+          { title: data?.data?.blog?.uid || "News Detail" },
         ]} />
       <BlogDescription blog={data?.data?.blog} />
       {Array.isArray(data?.data?.blog?.faq) && data?.data?.blog?.faq?.[0]?.question?.length > 0 && (
