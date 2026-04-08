9df2c87afe1868004f9d0c2625b9c652f2b69ef0
ajay <ajaykushwaha626162@gmail.com>
2026-04-08 11:48:40 +0530

    just revert

--- src/common-component/Header/Header.js
+++ src/common-component/Header/Header.js
@@ -249,7 +249,7 @@ const Header = () => {
     },
     // ── RMS SmartSet ──────────────────────────────────────────
     {
-      title: "RMSss",
+      title: "RMS",
       href: "/rms-entrance-exam",
       subtitles: [
         { title: "class 6", href: "/rms-entrance-exam/class-6" },
